310db2cb51b940e8fe631e7ec253fb05c2e3d696
Dauba <ab6060118@gmail.com>
2016-02-19 14:45:08 +0800

    [update] virtualbox

--- VirtualBox/install.sh
+++ VirtualBox/install.sh
@@ -17,8 +17,6 @@ if [ $(id -u) -eq 0 ]; then
     
     read -s -p "Enter password for new user: " password
     pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
-    sudo useradd -m -p $pass $username
-    [ $? -eq 0 ] && echo "\e[1;32mUser has been added to system!\e[0m" || echo "\e[1;31mFailed to add a user!\e[0m"
 else
     echo -e "\e[31mWaring: Only root can run this script.\e[0m"
     exit 1
@@ -29,7 +27,7 @@ echo "#                           Add repository                            #"
 echo "#######################################################################"
 echo
 
-sudo add-apt-repository "deb http://download.virtualbox.org/virtualbox/debian vivid contrib"
+add-apt-repository "deb http://download.virtualbox.org/virtualbox/debian vivid contrib"
 wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | apt-key add - -y
 
 echo
@@ -38,8 +36,8 @@ echo "#                         Install Virtualbox                          #"
 echo "#######################################################################"
 echo
 
-sudo apt-get update -y
-sudo apt-get install virtualbox-5.0 zip -y
+apt-get update -y
+apt-get install virtualbox-5.0 zip -y
 
 echo
 echo "#######################################################################"
@@ -53,7 +51,7 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-sudo VBoxManage extpack install Oracle_VM_VirtualBox_Extension_Pack-5.0.14-105127.vbox-extpack
+VBoxManage extpack install Oracle_VM_VirtualBox_Extension_Pack-5.0.14-105127.vbox-extpack
 if [ $? -ne 0 ]; then
     echo -e "\e[1;31mError: Install Virtaubox Extension fail.\e[0m"
     exit 1
@@ -67,7 +65,7 @@ echo "#                        Install PHPVirutalbox                        #"
 echo "#######################################################################"
 echo
 
-sudo apt-get install apache2 php5 php-soap zip -y
+apt-get install apache2 php5 php-soap zip -y
 
 wget http://sourceforge.net/projects/phpvirtualbox/files/phpvirtualbox-5.0-5.zip
 if [ $? -ne 0 ]; then
@@ -75,6 +73,22 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-unzip phpvirtualbox-5.0-5.zip
-sudo mv phpvirtualbox-5.0-5 /var/www/html/virtualbox
-rf -f phpvirtualbox-5.0-5.zip
+unzip phpvirtualbox-5.0-5.zip > /dev/null
+mv phpvirtualbox-5.0-5 /var/www/html/virtualbox
+rm -f phpvirtualbox-5.0-5.zip
+
+echo
+echo "#######################################################################"
+echo "#            Create user anda add user to vboxusers group             #"
+echo "#######################################################################"
+echo
+
+useradd -m -p $pass -G $vboxusersGoup $username
+[ $? -eq 0 ] && echo -e "\e[1;32m$username has been added to $vboxusersGoup!\e[0m" && exit 1 || echo -e "\e[1;31mFailed to add a user!\e[0m"
+
+echo "VBOXWEB_USER=$username" > /etc/default/virtualbox
+[ $? -ne 0 ] && echo -e "\e[1;32mCreate /etc/default/virtualbox fail.\e[0m" && exit 1 || echo -e "\e[1;32mCreate /etc/default/virtualbox success.\e[0m"
+
+
+service vboxweb-service start
+[ $? -ne 0 ] && exit 1
